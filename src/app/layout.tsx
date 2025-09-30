470ad2a3d3268a941d6c124e540e4790546e0c81
tteuw00111111 <tteuw021@proton.me>
2025-09-30 11:21:54 -0300

    updates gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,9 +108,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className="!scroll-smooth">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P2FKHVJV');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${inter.variable} ${poppins.variable} ${roboto.variable} font-inter bg-[#0A0A0A] text-gray-200 antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2FKHVJV"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* 1. Adicione o script do Cookiebot AQUI, antes de tudo */}
         <Script
           id="Cookiebot"
